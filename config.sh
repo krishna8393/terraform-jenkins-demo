f21769387633bf3813349def72423816dd8d57e2
Raghava-devops <41502228+Raghava-devops@users.noreply.github.com>
2022-02-16 22:19:59 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,1 +1,1 @@
-aws sts assume-role --role-arn arn:aws:iam::$accountid:role/service/$rolename --role-session-name jenkins-agent --duration-seconds 3600 --query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" --output text
+aws sts assume-role --role-arn arn:aws:iam::$accountid:role/$rolename --role-session-name jenkins-agent --duration-seconds 3600 --query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" --output text
