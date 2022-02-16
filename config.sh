f9ab4930a8b864c1912551969a2d0433f4ef32ec
Raghava-devops <41502228+Raghava-devops@users.noreply.github.com>
2022-02-16 22:23:22 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,1 +1,2 @@
 aws sts assume-role --role-arn arn:aws:iam::$accountid:role/$rolename --role-session-name jenkins-agent --duration-seconds 3600 --query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" --output text
+aws sts get-caller-identity
