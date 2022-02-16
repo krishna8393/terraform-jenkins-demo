b8f84fac477c07cd14223bc73b82fdaa4d605249
Raghava-devops <41502228+Raghava-devops@users.noreply.github.com>
2022-02-16 23:32:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,23 @@ provider "aws" {
   region  = "ap-south-1"
 }
 
-resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
+resource "aws_iam_role" "test_role" {
+  name = "terraform_jenkins_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
 
   tags = {
-    Name = "main"
+    tag-key = "tag-value"
   }
 }
