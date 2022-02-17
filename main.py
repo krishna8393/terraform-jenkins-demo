530b21dfb423b3c759d6951c4f2812c8eececd97
Raghava-devops <41502228+Raghava-devops@users.noreply.github.com>
2022-02-17 19:27:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import boto3
 client = boto3.client('iam')
 def lambda_handler(event, context):
-    response = client.create_role(
-        Path='string',
-        RoleName='string',
-        AssumeRolePolicyDocument='string',
-        Description='string',
-        MaxSessionDuration=123,
-        PermissionsBoundary='string',
-        Tags=[
-            {
-                'Key': 'string',
-                'Value': 'string'
-            },
-        ]
-    )
+    response = client.create_user(
+    Path='/',
+    UserName='terraform',
+    Tags=[
+        {
+            'from': 'terraform'
+        },
+    ]
+)
