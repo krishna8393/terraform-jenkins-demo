b1f604ed6ec0527f0175abe76f4e334311510a8e
Raghava-devops <41502228+Raghava-devops@users.noreply.github.com>
2022-02-17 23:07:23 +0530

    Update config.sh

--- shell/config.sh
+++ shell/config.sh
@@ -6,8 +6,8 @@ printf "AWS_ACCESS_KEY_ID=%s AWS_SECRET_ACCESS_KEY=%s AWS_SESSION_TOKEN=%s" $(
 aws sts assume-role --role-arn arn:aws:iam::$accountid:role/$rolename --role-session-name jenkins-agent --duration-seconds 3600 --query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" --output text
 )
 )
-cd ../bin/
-python3 main.py
+
+python3 $path/bin/main.py
 
 # terraform init
 # terraform plan
