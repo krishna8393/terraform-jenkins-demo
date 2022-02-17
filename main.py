a6343f49a6f3390043f9595d46abdc2dcdf47507
Raghava-devops <41502228+Raghava-devops@users.noreply.github.com>
2022-02-17 22:21:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,7 @@ client = boto3.client('iam')
 def lambda_handler():
     response = client.create_user(
     Path='/',
-    UserName='terraform',
-    Tags=[
-        {
-            'from': 'terraform'
-        },
-    ]
-)
+    UserName='terraform')
+ 
 if __name__ == "__main__":
     lambda_handler()
