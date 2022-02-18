ddc6900c7ec2e74de9f702ef16a6c507fc96fd0c
Raghava-devops <41502228+Raghava-devops@users.noreply.github.com>
2022-02-18 16:34:48 +0530

    Update main.py

--- bin/main.py
+++ bin/main.py
@@ -4,6 +4,7 @@ def lambda_handler():
     response = client.create_user(
     Path='/',
     UserName='terraform')
+    print('response', response)
  
 if __name__ == "__main__":
     lambda_handler()
